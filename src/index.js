26ac5086f02c62b3545284deabd81132742ec941
M. Yousaf Ramzan <yousaf0811@gmail.com>
2022-12-24 12:45:47 +0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+
+const authHandler = require("./middleware/auth");
+
+const User = require("./models/user");
+
+const router = require("./routes/user/user.controller");
+
+const app = express();
+
+app.use(express.json());
+app.use(authHandler);
+
+mongoose
+  .connect("mongodb://localhost:27017/bootcamp")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.log(`Couldn't connected to MongoDB, ${error}`));
+
+app.use("/users", router);
+
+
+app.listen(5000, () => console.log("App is listening at port 5000"));
